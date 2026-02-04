f34d52ef2d6ec9d4c9e05ee44e6cc04db214c0ed
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-02-04 13:20:57 +0300

    creating the memory still in place is i assert dominance

--- memory.puzzle.py
+++ memory.puzzle.py
@@ -9,4 +9,5 @@ WINDOWHEIGHT = 480 # THE HEIGHT IN PIXELS OF THE WINDOW OFC
 REVEALSPEED = 8 # speed of the boxes sliding to reveal the hidden content 
 BOXSIZE = 40 # size of the box height and width in pixels
 GAPSIZE = 10 # size og the gap between the boxes in pixels
-
+BOARDWIDTH = 10 # number of the columns of icons 
+BOARDHEIGHT = 7 # number of rows of ico
