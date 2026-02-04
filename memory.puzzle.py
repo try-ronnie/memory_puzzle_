9bca168cd126f33e7cf9b84360364ee8433d1518
Ronnie Gikandi <kiiruronnie42@gmail.com>
2026-02-04 13:17:57 +0300

    creating the memory for the actions speed of thing and the memory for the size of the boxes and how the game will run

--- memory.puzzle.py
+++ memory.puzzle.py
@@ -1,3 +1,12 @@
 import random , pygame , sys
 from pygame.locals import *
+# first we write the memory required to run this game 
+
+
+fps = 30 # general speed of the program
+WINDOWWIDTH = 640 # height of the windows width in pixels 
+WINDOWHEIGHT = 480 # THE HEIGHT IN PIXELS OF THE WINDOW OFC
+REVEALSPEED = 8 # speed of the boxes sliding to reveal the hidden content 
+BOXSIZE = 40 # size of the box height and width in pixels
+GAPSIZE = 10 # size og the gap between the boxes in pixels
 
